9b79012c4e5909672cfa927e84a6eef8a1e9c7ab
JaoodxD <jaood96@gmail.com>
2022-12-06 16:34:19 +0200

    update ESModule

--- index.mjs
+++ index.mjs
@@ -1,85 +1,34 @@
 //@ts-check
-let countryCode = {
-    'UA': { code: "380", maxLen: 12, mask: '+## ### ### ####', codes: ['0', '80', '380', '38'] }, //+38 096 555 55 55
-    'KZ': { code: '7', maxLen: 11, mask: '+# ### ### ####', codes: ['7', '8'] }, //+7 777 001 44 99
-    'GLOBAL': { code: '', minLen: 5, maxLen: 13, mask: '+#############', codes: [] }
-};
+import operatorPrefixes from './operator-prefixes.json';
+
+import countryCode from './countries-metada.json';
 /**
  * Function to return country name by international code 
  * @param {String|Number} code  String or numeric representation of country prefix
  * @returns Alpha‑2 code country name if found otherwise 'GLOBAL'  
  */
-// @ts-ignore
-let getCountry = code => Object.entries(countryCode).find(([key, value]) => value.code == code)?.[0] ?? 'GLOBAL';
-let operatorsUA = {
-    'Kyivstar': [
-        '067',
-        '068',
-        '096',
-        '097',
-        '098',
-    ],
-    'Vodafone': [
-        '050',
-        '066',
-        '095',
-        '099'
-    ],
-    'lifecell': [
-        '063',
-        '073',
-        '093',
-    ]
-}
-let operatorsKZ = {
-    'Activ': [
-        '727',
-        '701',
-        '702',
-        '772',
-        '778',
-    ],
-    'Altel': [
-        '700',
-        '708',
-    ],
-    'Beeline': [
-        '705',
-        '771',
-        '776',
-        '777'
-    ],
-    'Tele2': [
-        '707',
-        '747',
-    ]
-}
-let operatorPrefixes = {
-    'UA': operatorsUA,
-    'KZ': operatorsKZ
-}
+export const getCountry = (code) => Object.entries(countryCode)
+    .find(([, value]) =>
+        value.code == code)?.[0] ?? 'GLOBAL';
 
-// @ts-ignore
-let proxying = map => new Proxy(Object.entries(map).reduce((obj, [key, value]) => ({ ...obj, [value.join('|')]: key }), {}), {
-    // @ts-ignore
-    get: (target, property, reciever) => {
+
+const proxying = (map) => new Proxy(Object.entries(map).reduce((obj, [key, value]) => ({ ...obj, [value.join('|')]: key }), {}), {
+    get: (target, property) => {
         for (let k in target)
             if (new RegExp(k).test(property.toString()))
-                // @ts-ignore
                 return target[k];
         return null;
     }
-})
-let proxy = Object.entries(operatorPrefixes).reduce((obj, [key, value]) => ({ ...obj, [key]: proxying(value) }), {});
+});
+const proxy = Object.entries(operatorPrefixes).reduce((obj, [key, value]) => ({ ...obj, [key]: proxying(value) }), {});
 /**
  * 
  * @param {String} code 
  * @param {'UA'|'KZ'|String} country 
  * @returns 
  */
-// @ts-ignore
-let recognize = (code, country) => proxy[country][code];
-let localOperatorIcons = {
+const recognize = (code, country) => proxy[country][code];
+const localOperatorIcons = {
     'UA': {
         'Kyivstar': 'icon-Union-1',
         'Vodafone': 'icon-Vector-1',
@@ -96,10 +45,9 @@ let localOperatorIcons = {
     'UNKNOWN': 'icon-Union',
     'INCORRECT': 'icon-Union-18',
     'EMPTY': ''//'icon-uniE941',
-}
+};
 
-// @ts-ignore
-let normalizeCountryName = country => {
+const normalizeCountryName = (country) => {
     switch (country) {
         case 'Украина':
         case 'UA':
@@ -116,18 +64,18 @@ let normalizeCountryName = country => {
             break;
     }
     return country;
-}
+};
 /**
  * 
  * @param {String} phone number to be masked
  * @param {String} pattern pattern to mask inputted number which contains '#' as placceholder 
  * @returns masked phone number e.g. value=123453, pattern='+### # ##' will gives '+123 4 53'
  */
-let mask = (phone = '', pattern = '+#############') => {
+const mask = (phone = '', pattern = '+#############') => {
     let i = 0;
     const v = phone.toString();
     return pattern.replace(/#/g, _ => v[i++] ?? '').trimEnd();
-}
+};
 /**
  * Function of converting mobile number to international format e.g. phone = '0965558844' and country = 'UA' -> +38 096 555 88 44
  * @param {String} phone 
@@ -135,7 +83,7 @@ let mask = (phone = '', pattern = '+#############') => {
  * @param {'UA'| 'KZ'| 'GLOBAL' | undefined} prevCountry
  * @returns {String} Formatted 
  */
-let formatPhone = (phone, country, prevCountry = undefined) => {
+export const formatPhone = (phone, country, prevCountry = undefined) => {
     if (phone) {
 
         country = normalizeCountryName(country);
@@ -164,14 +112,14 @@ let formatPhone = (phone, country, prevCountry = undefined) => {
         return phone;
     }
     return '';
-}
+};
 /**
  *  Mobile operator icon recognition function by phone number
  * @param {String} phone 
  * @param {'UA'| 'KZ'| 'GLOBAL'} country 
  * @returns {String} icon name of specific recognized mobile operator
  */
-let recognizeOperator = (phone, country = 'GLOBAL') => {
+export const recognizeOperator = (phone, country = 'GLOBAL') => {
     country = normalizeCountryName(country);
     phone = formatPhone(phone, country);
     let currentCountryCode = countryCode[country];
@@ -180,26 +128,22 @@ let recognizeOperator = (phone, country = 'GLOBAL') => {
     //counting phone digits
     let numLen = (phone.match(/\d/g) || []).length;
     if (country == 'GLOBAL') {
-        // @ts-ignore
         if (numLen >= currentCountryCode.minLen && numLen <= currentCountryCode.maxLen)
             return localOperatorIcons.UNKNOWN;
     }
     if (numLen != currentCountryCode.maxLen) {
         return localOperatorIcons.INCORRECT
     }
-    // @ts-ignore
-    let { int, operator } = phone.replace('+', '').match(/(?<int>^\d+) (?<operator>\d+)/)?.groups;
+    //@ts-ignore
+    let { int, operator } = phone
+        .replace('+', '')
+        .match(/(?<int>^\d+) (?<operator>\d+)/)
+        ?.groups;
     if (!!operator && !!int) {
         operator = recognize(operator, country);
         if (operator)
-            // @ts-ignore
             return localOperatorIcons[country][operator];
         return localOperatorIcons.UNKNOWN;
     }
     return localOperatorIcons.INCORRECT;
-}
-export {
-    getCountry,
-    formatPhone,
-    recognizeOperator
-}
+};
