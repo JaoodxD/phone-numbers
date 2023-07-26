b74e883f352d8b515acdc50350d8f86e83abcdb2
JaoodxD <jaood96@gmail.com>
2023-07-26 23:13:32 +0300

    Apply `standard` format fix

--- test/phone-numbers.test.js
+++ test/phone-numbers.test.js
@@ -1,21 +1,21 @@
-'use strict';
+'use strict'
 
-const test = require('node:test');
-const assert = require('node:assert');
-const config = require('..');
+const test = require('node:test')
+const assert = require('node:assert')
+const config = require('..')
 
 test('simple lib configuring', async (t) => {
-  assert.doesNotThrow(() => config([]));
-});
+  assert.doesNotThrow(() => config([]))
+})
 
 test('format UA number', async (t) => {
-  const countries = [{ ISO: 'UA', countryCode: '380', mask: '## ### ### ####' }];
-  const { formatPhone } = config(countries);
-  const initalNumber = '+380965556677';
-  const result = formatPhone(initalNumber, 'UA');
-  const expected = '+38 096 555 6677';
-  assert.equal(result, expected);
-});
+  const countries = [{ ISO: 'UA', countryCode: '380', mask: '## ### ### ####' }]
+  const { formatPhone } = config(countries)
+  const initalNumber = '+380965556677'
+  const result = formatPhone(initalNumber, 'UA')
+  const expected = '+38 096 555 6677'
+  assert.equal(result, expected)
+})
 
 test('recognize operator by number and country', async (t) => {
   const countries = [{
@@ -23,10 +23,10 @@ test('recognize operator by number and country', async (t) => {
     countryCode: '380',
     mask: '## ### ### ####',
     operators: [{ prefixes: ['96'], name: 'Kyivstar' }]
-  }];
-  const { recognizeOperator } = config(countries);
-  const initalNumber = '+380965556677';
-  const result = recognizeOperator(initalNumber, 'UA');
-  const expected = 'Kyivstar';
-  assert.equal(result, expected);
-});
+  }]
+  const { recognizeOperator } = config(countries)
+  const initalNumber = '+380965556677'
+  const result = recognizeOperator(initalNumber, 'UA')
+  const expected = 'Kyivstar'
+  assert.equal(result, expected)
+})
