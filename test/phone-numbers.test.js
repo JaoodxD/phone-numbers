3fb3baf0ec6bde2219fc7f74a785bf0d7f7c34fe
JaoodxD <jaood96@gmail.com>
2023-07-26 16:42:01 +0300

    Add tests to lib with new implementation and contract

--- test/phone-numbers.test.js
+++ test/phone-numbers.test.js
@@ -5,13 +5,28 @@ const assert = require('node:assert');
 const config = require('..');
 
 test('simple lib configuring', async (t) => {
-  assert.doesNotThrow(config);
+  assert.doesNotThrow(() => config([]));
 });
 
 test('format UA number', async (t) => {
-  const { formatPhone } = config();
-  const inital = '+380965556677';
-  const result = formatPhone(inital, 'UA');
+  const countries = [{ ISO: 'UA', countryCode: '380', mask: '## ### ### ####' }];
+  const { formatPhone } = config(countries);
+  const initalNumber = '+380965556677';
+  const result = formatPhone(initalNumber, 'UA');
   const expected = '+38 096 555 6677';
+  assert.equal(result, expected);
+});
+
+test('recognize operator by number and country', async (t) => {
+  const countries = [{
+    ISO: 'UA',
+    countryCode: '380',
+    mask: '## ### ### ####',
+    operators: [{ prefixes: ['96'], name: 'Kyivstar' }]
+  }];
+  const { recognizeOperator } = config(countries);
+  const initalNumber = '+380965556677';
+  const result = recognizeOperator(initalNumber, 'UA');
+  const expected = 'Kyivstar';
   assert.equal(result, expected);
 });
