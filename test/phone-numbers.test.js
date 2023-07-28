16d274b669caade66d393b075402e8fab386f0cb
JaoodxD <jaood96@gmail.com>
2023-07-28 11:45:48 +0300

    Add tests

--- test/phone-numbers.test.js
+++ test/phone-numbers.test.js
@@ -30,3 +30,60 @@ test('recognize operator by number and country', async (t) => {
   const expected = 'Kyivstar'
   assert.equal(result, expected)
 })
+
+test('return uknown when operator is not present in country', async (t) => {
+  const countries = [{
+    ISO: 'UA',
+    countryCode: '380',
+    mask: '## ### ### ####',
+    operators: [{ prefixes: ['96'], name: 'Kyivstar' }]
+  }]
+  const { recognizeOperator } = config(countries)
+  const initalNumber = '+380775556677'
+  const result = recognizeOperator(initalNumber, 'UA')
+  const expected = 'unknown'
+  assert.equal(result, expected)
+})
+
+test('recognize operator with formatted number', async (t) => {
+  const countries = [{
+    ISO: 'UA',
+    countryCode: '380',
+    mask: '## ### ### ####',
+    operators: [{ prefixes: ['96'], name: 'Kyivstar' }]
+  }]
+  const { recognizeOperator } = config(countries)
+  const initalNumber = '+38 096 555 6677'
+  const result = recognizeOperator(initalNumber, 'UA')
+  const expected = 'Kyivstar'
+  assert.equal(result, expected)
+})
+
+test('format on input', async (t) => {
+  const countries = [{
+    ISO: 'UA',
+    countryCode: '380',
+    mask: '## ### ### ####',
+    operators: [{ prefixes: ['96'], name: 'Kyivstar' }]
+  }]
+  const { formatPhone } = config(countries)
+  const table = [
+    ['+380965556677', '+38 096 555 6677'],
+    ['+38096555667', '+38 096 555 667'],
+    ['+3809655566', '+38 096 555 66'],
+    ['+380965556', '+38 096 555 6'],
+    ['+38096555', '+38 096 555'],
+    ['+3809655', '+38 096 55'],
+    ['+380965', '+38 096 5'],
+    ['+38096', '+38 096'],
+    ['+3809', '+38 09'],
+    ['+380', '+380'],
+    ['+38', '+38'],
+    ['+3', '+3'],
+    ['+', '+']
+  ]
+  for (const [input, expected] of table) {
+    const result = formatPhone(input, 'UA')
+    assert.equal(result, expected)
+  }
+})
